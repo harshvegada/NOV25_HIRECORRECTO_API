a2cc791d61c784c820e9d556681ed067f51cebff
HarshPatel <58640315+harshvegada@users.noreply.github.com>
2026-01-04 09:44:41 +0530

    Delete src directory

--- src/main/java/base/ScreeningControl.java
+++
@@ -1,9 +0,0 @@
-package base;
-
-public class ScreeningControl {
-
-    public static String candidateScreeningId;
-    public static String jobRoleID;
-    public static String jobApplicationID;
-
-}
--- src/main/java/constants/FilePaths.java
+++
@@ -1,8 +0,0 @@
-package constants;
-
-public class FilePaths {
-
-    public static final String SCHEMA_FILE_FOR_VIDEO_AUDIO_MCQ = "src/test/resources/schemaFiles/video_aduio_mcq_Schema_Files.json";
-    public static final String SCHEMA_FILE_FOR_SUBJECTIVE_PROGRAMMING = "src/test/resources/schemaFiles/subjectiveAndProgrammingSchemaFile.json";
-
-}
